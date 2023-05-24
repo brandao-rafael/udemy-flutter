0215ef846cf98f24e3e7d40671a1b88e5018bb29
Rafael <rafaelbrandao1992@gmail.com>
2023-05-24 16:54:22 -0300

    refactor: change stateless to statefull

--- sessao-2-nocoes-basicas-de-flutter/project_questions/lib/main.dart
+++ sessao-2-nocoes-basicas-de-flutter/project_questions/lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(PerguntaApp());
 
+class PerguntaAppState extends State<PerguntaApp> {
+  var selectedAwnser = 0;
 
-class PerguntaApp extends StatelessWidget {
+  void awnser() {
+    setState(() {
+      selectedAwnser += 1;
+    });
+    print(selectedAwnser);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final List<String> questions = [
+      'Qual a sua cor favorita?',
+      'Qual o seu animal favorito?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -14,8 +25,33 @@ class PerguntaApp extends StatelessWidget {
             child: Text('Questions')
           ),
         ),
-        body: const Text('Hello world'),
+        body: Column(
+          children: [
+            Text(questions[selectedAwnser]),
+            ElevatedButton(
+              onPressed: awnser,
+              child: Text('resposta 1')
+            ),
+            ElevatedButton(
+              onPressed: awnser,
+              child: Text('resposta 2')
+            ),
+            ElevatedButton(
+              onPressed: awnser,
+              child: Text('resposta 3')
+            ),
+          ]
+        ),
       )
     );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
