d58652331dea1c0b54f6569cc251978c7d391891
Rafael <rafaelbrandao1992@gmail.com>
2023-05-29 16:40:59 -0300

    feat: conditional to render image and text with no expenses

--- sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
+++ sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
@@ -11,7 +11,25 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
+      child: transactions.isEmpty ? 
+        Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              'Nenhuma Transação Cadastrada',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 200,
+              child: Image.asset(
+                'assets/images/waiting.png',
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        )
+      : ListView.builder(
         itemCount: transactions.length,
         itemBuilder: (ctx, index) {
           final tr = transactions[index];
--- sessao-4-contruindo-app-real/expenses/lib/main.dart
+++ sessao-4-contruindo-app-real/expenses/lib/main.dart
@@ -45,18 +45,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo tenis de corrida',
-      value: 310.76,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 211.30,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo tenis de corrida',
+    //   value: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Conta de luz',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   _addTransaction(String title, double value) {
