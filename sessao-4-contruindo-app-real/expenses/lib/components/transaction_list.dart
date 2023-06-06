455580cff8b5f6da5c97c653552f076377a5e3b5
Rafael <rafaelbrandao1992@gmail.com>
2023-06-06 17:46:23 -0300

    refactor: change height of chart and list

--- sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
+++ sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return transactions.isEmpty
         ? LayoutBuilder(
-          builder: (context, constraints) => Column(
+            builder: (context, constraints) => Column(
               children: [
                 const SizedBox(height: 20),
                 Text(
@@ -29,45 +29,48 @@ class TransactionList extends StatelessWidget {
                 )
               ],
             ),
-        )
+          )
         : ListView.builder(
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
               return Card(
                 elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5
-                ),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                 child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Theme.of(context).colorScheme.primary,
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(
-                          child: Text(
-                        'R\$${tr.value}',
-                        style: const TextStyle(
-                          color: Colors.white
-                        ),
-                      )),
+                    leading: CircleAvatar(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                            child: Text(
+                          'R\$${tr.value.toStringAsFixed(2)}',
+                          style: const TextStyle(color: Colors.white),
+                        )),
+                      ),
                     ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Theme.of(context).colorScheme.error,
-                    onPressed: () => onRemove(tr.id),
-                  ),
-                ),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: MediaQuery.of(context).size.width > 480
+                        ? TextButton.icon(
+                            onPressed: () => {},
+                            icon: const Icon(Icons.delete),
+                            label: const Text('Excluir'),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Theme.of(context).colorScheme.error,
+                            ),
+                          )
+                        : IconButton(
+                            icon: const Icon(Icons.delete),
+                            color: Theme.of(context).colorScheme.error,
+                            onPressed: () => onRemove(tr.id),
+                          )),
               );
             },
           );
--- sessao-4-contruindo-app-real/expenses/lib/main.dart
+++ sessao-4-contruindo-app-real/expenses/lib/main.dart
@@ -121,12 +121,12 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             if (_showChart || !isLandscape)
             SizedBox(
-              height: availableHeight * (isLandscape ? 0.7 : 0.3),
+              height: availableHeight * (isLandscape ? 0.8 : 0.3),
               child: Chart(_recentTransaction),
             ),
             if (!_showChart || !isLandscape)
             SizedBox(
-              height: availableHeight * 0.7,
+              height: availableHeight * (isLandscape ? 1 : 0.7),
               child: TransactionList(_transactions, _removeTransaction),
             ),
           ],
