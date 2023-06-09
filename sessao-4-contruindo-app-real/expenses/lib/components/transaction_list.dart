b79e642eecf4523d72c49a9832673d79b94eabc0
Rafael <rafaelbrandao1992@gmail.com>
2023-06-09 14:55:25 -0300

    refactor: componetizing listItem

--- sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
+++ sessao-4-contruindo-app-real/expenses/lib/components/transaction_list.dart
@@ -1,6 +1,6 @@
+import 'package:expenses/components/list_item.dart';
 import 'package:expenses/models/transaction_model.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -34,43 +34,9 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Theme.of(context).colorScheme.primary,
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                            child: Text(
-                          'R\$${tr.value.toStringAsFixed(2)}',
-                          style: const TextStyle(color: Colors.white),
-                        )),
-                      ),
-                    ),
-                    title: Text(
-                      tr.title,
-                      style: Theme.of(context).textTheme.titleLarge,
-                    ),
-                    subtitle: Text(
-                      DateFormat('d MMM y').format(tr.date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 480
-                        ? TextButton.icon(
-                            onPressed: () => {},
-                            icon: const Icon(Icons.delete),
-                            label: const Text('Excluir'),
-                            style: TextButton.styleFrom(
-                              foregroundColor: Theme.of(context).colorScheme.error,
-                            ),
-                          )
-                        : IconButton(
-                            icon: const Icon(Icons.delete),
-                            color: Theme.of(context).colorScheme.error,
-                            onPressed: () => onRemove(tr.id),
-                          )),
+              return ListItem(
+                tr: tr,
+                onRemove: onRemove,
               );
             },
           );
