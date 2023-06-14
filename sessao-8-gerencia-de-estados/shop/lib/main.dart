d82c7162f243ea95d7ba913f5aefd9305c8f4c6a
Rafael <rafaelbrandao1992@gmail.com>
2023-06-14 15:15:44 -0300

    feat: create initial product detail page

--- sessao-8-gerencia-de-estados/shop/lib/main.dart
+++ sessao-8-gerencia-de-estados/shop/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
+import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +16,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shop',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
         fontFamily: 'Lato',
         colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.purple,
           secondary: Colors.deepOrange,
         )
         
       ),
       home: ProductsOverviewPage(),
       debugShowCheckedModeBanner: false,
+      routes: {
+        AppRoutes.PRODUCT_DETAIL:(ctx) => const ProductDetailPage(),
+      },
     );
   }
 }
