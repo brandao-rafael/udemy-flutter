defa9769fb1a3347abb4b7854f61145ccc92629a
Rafael <rafaelbrandao1992@gmail.com>
2023-06-14 17:54:37 -0300

    feat: implementing favorite logics

--- sessao-8-gerencia-de-estados/shop/lib/pages/products_overview_page.dart
+++ sessao-8-gerencia-de-estados/shop/lib/pages/products_overview_page.dart
@@ -1,14 +1,47 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/product_list.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
 class ProductsOverviewPage extends StatelessWidget {
-  
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Minha Loja')),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedValue) {
+              if(selectedValue == FilterOptions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorite,
+                child: Text('Somente Favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Todos'),
+              ),
+            ],
+          )
+        ],
       ),
       body: const ProductGrid(),
     );
