59555ea2ea03418740e4e6ed0493628c3d7bedd9
Rafael <rafaelbrandao1992@gmail.com>
2023-06-15 16:15:27 -0300

    feat: fill orderWidget

--- sessao-8-gerencia-de-estados/shop/lib/components/order.dart
+++ sessao-8-gerencia-de-estados/shop/lib/components/order.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 
 import 'package:shop/models/order.dart';
 
-class OrderWidget extends StatelessWidget {
+class OrderWidget extends StatefulWidget {
   final Order order;
   const OrderWidget({
     Key? key,
@@ -12,19 +12,61 @@ class OrderWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
+
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
+  @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yyyy hh:mm').format(order.date)
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.expand_more),
-          onPressed: () {
-            
-          },
-        ),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+            subtitle:
+                Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.date)),
+            trailing: IconButton(
+              icon: const Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: (widget.order.products.length * 25.0) + 10,
+              child: ListView(
+                children: widget.order.products.map((e) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        e.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        '${e.quantity}x ${e.price}',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.grey
+                        ),
+                      )
+                    ],
+                  );
+                }).toList(),
+              ),
+            )
+        ],
       ),
     );
   }
