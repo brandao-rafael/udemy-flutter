f750c2f853d8c35e78ce962d0fa1d853729361cd
Rafael <rafaelbrandao1992@gmail.com>
2023-06-15 16:02:26 -0300

    feat: create component OrderWidget

--- sessao-8-gerencia-de-estados/shop/lib/components/app_drawer.dart
+++ sessao-8-gerencia-de-estados/shop/lib/components/app_drawer.dart
@@ -11,6 +11,7 @@ class AppDrawer extends StatelessWidget {
         children: [
           AppBar(
             title: const Text('Bem vindo Usuário'),
+            automaticallyImplyLeading: false,
           ),
           const Divider(),
           ListTile(
@@ -23,7 +24,7 @@ class AppDrawer extends StatelessWidget {
           const Divider(),
           ListTile(
             leading: const Icon(Icons.payment),
-            title: const Text('Loja'),
+            title: const Text('Pedidos'),
             onTap: () {
               Navigator.of(context).pushReplacementNamed(AppRoutes.ORDERS);
             },
