97fc055fe3cedd696b04999ddeaf143bfee69ea9
Rafael <rafaelbrandao1992@gmail.com>
2023-06-15 14:54:35 -0300

    feat: layout cart item

--- sessao-8-gerencia-de-estados/shop/lib/pages/cart_page.dart
+++ sessao-8-gerencia-de-estados/shop/lib/pages/cart_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
 import 'package:shop/models/cart.dart';
 
 class CartPage extends StatelessWidget {
@@ -8,6 +9,8 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Carrinho')),
@@ -15,7 +18,7 @@ class CartPage extends StatelessWidget {
       body: Column(
         children: [
           Card(
-            margin: const EdgeInsets.all(25),
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
             child: Padding(
               padding: const EdgeInsets.all(10),
               child: Row(
@@ -44,14 +47,18 @@ class CartPage extends StatelessWidget {
                   TextButton(
                     onPressed: () {},
                     style: TextButton.styleFrom(
-                      textStyle: TextStyle(
-                        color: Theme.of(context).colorScheme.primary
-                      )
-                    ),
+                        textStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.primary)),
                     child: const Text('COMPRAR'),
                   )
                 ],
               ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, i) => CartItemWidget(items[i]),
             ),
           )
         ],
