51dad68665eb7754c970b4c840be54f183840c09
Rafael <rafaelbrandao1992@gmail.com>
2023-06-15 17:30:26 -0300

    feat: implementing product manager page

---
+++ sessao-8-gerencia-de-estados/shop/lib/components/product_item.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem(
+    this.product, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      title: Text(product.name),
+      trailing: Container(
+        width: 100,
+        child: Row(children: [
+          IconButton(
+            icon: Icon(Icons.edit),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ]),
+      ),
+    );
+  }
+}
