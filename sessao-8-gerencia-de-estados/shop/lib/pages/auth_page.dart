4bf0cd93de3f8f1f4c38ef1a76a2185363e56579
Rafael <rafaelbrandao1992@gmail.com>
2023-06-23 17:49:12 -0300

    feat: resolving responsivity for auth page

--- sessao-8-gerencia-de-estados/shop/lib/pages/auth_page.dart
+++ sessao-8-gerencia-de-estados/shop/lib/pages/auth_page.dart
@@ -22,42 +22,46 @@ class AuthPage extends StatelessWidget {
               end: Alignment.bottomRight,
             )),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 70,
-                  ),
-                  // cascade operator
-                  transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: const [
-                      BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0, 2)
-                      )
-                    ],
-                    color: Colors.deepOrange.shade900
-                  ),
-                  child: Text(
-                    "Minha loja",
-                    style: TextStyle(
-                      fontSize: 45,
-                      fontFamily: 'Anton',
-                      color: Theme.of(context).primaryTextTheme.headlineLarge?.color
+          Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 70,
+                      ),
+                      // cascade operator
+                      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            blurRadius: 8,
+                            color: Colors.black26,
+                            offset: Offset(0, 2)
+                          )
+                        ],
+                        color: Colors.deepOrange.shade900
+                      ),
+                      child: Text(
+                        "Minha loja",
+                        style: TextStyle(
+                          fontSize: 45,
+                          fontFamily: 'Anton',
+                          color: Theme.of(context).primaryTextTheme.headlineLarge?.color
+                        ),
+                      ),
                     ),
-                  ),
+                    const AuthForm(),
+                  ],
                 ),
-                const AuthForm(),
-              ],
+              ),
             ),
           )
         ],
