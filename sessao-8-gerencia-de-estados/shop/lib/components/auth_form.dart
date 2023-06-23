2503681292171d879125173b9a7761316df771ca
Rafael <rafaelbrandao1992@gmail.com>
2023-06-23 17:05:20 -0300

    feat: animate auth page

--- sessao-8-gerencia-de-estados/shop/lib/components/auth_form.dart
+++ sessao-8-gerencia-de-estados/shop/lib/components/auth_form.dart
@@ -15,26 +15,55 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final _passwordController = TextEditingController();
   final _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
   AuthMode _authMode = AuthMode.Login;
+  // ignore: prefer_final_fields
   Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
+  AnimationController? _controller;
+  Animation<Size>? _heigthAnimation;
 
   bool _isLogin() => _authMode == AuthMode.Login;
   bool _isSignup() => _authMode == AuthMode.Signup;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 300));
+
+    _heigthAnimation = Tween(
+      begin: const Size(double.infinity, 310),
+      end: const Size(double.infinity, 400),
+    ).animate(CurvedAnimation(
+      parent: _controller!,
+      curve: Curves.linear,
+    ));
+
+    _heigthAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   void _switchAuthMode() {
     setState(() {
       if (_isLogin()) {
         _authMode = AuthMode.Signup;
+        _controller?.forward();
       } else {
         _authMode = AuthMode.Login;
+        _controller?.reverse();
       }
     });
   }
@@ -81,7 +110,7 @@ class _AuthFormState extends State<AuthForm> {
       }
     } on AuthException catch (e) {
       _showErrorDialog(e.toString());
-    } catch(_) {
+    } catch (_) {
       _showErrorDialog('Ocorreu um erro inesperado');
     }
 
@@ -96,7 +125,8 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Container(
         padding: const EdgeInsets.all(16),
-        height: _isLogin() ? 310 : 400,
+        // height: _isLogin() ? 310 : 400,
+        height: _heigthAnimation?.value.height ?? (_isLogin() ? 310 : 400),
         width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
