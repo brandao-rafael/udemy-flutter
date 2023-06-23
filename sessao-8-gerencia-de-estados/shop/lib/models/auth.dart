8e3585021470dafd7ecdf073ea1a3ebd7c67e75f
Rafael <rafaelbrandao1992@gmail.com>
2023-06-23 15:49:44 -0300

    feat: implementing logout

--- sessao-8-gerencia-de-estados/shop/lib/models/auth.dart
+++ sessao-8-gerencia-de-estados/shop/lib/models/auth.dart
@@ -19,48 +19,53 @@ class Auth with ChangeNotifier {
     return isAuth ? _token : null;
   }
 
-    String? get email {
+  String? get email {
     return isAuth ? _email : null;
   }
-  
+
   String? get userId {
     return isAuth ? _userId : null;
   }
 
-  Future<void> _authenticate(String email, String password, String urlFragment) async {
-    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyCk-ovKfq1QJUxkHZgI4KS1yATkJdIFipA';
-    final response = await http.post(
-      Uri.parse(url),
-      body: jsonEncode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      })
-    );
+  Future<void> _authenticate(
+      String email, String password, String urlFragment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlFragment?key=AIzaSyCk-ovKfq1QJUxkHZgI4KS1yATkJdIFipA';
+    final response = await http.post(Uri.parse(url),
+        body: jsonEncode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }));
 
     final body = jsonDecode(response.body);
 
-    if(body['error'] != null) {
+    if (body['error'] != null) {
       throw AuthException(body['error']['message']);
     } else {
       _token = body['idToken'];
       _email = body['email'];
       _userId = body['localId'];
-      _expiryDate = DateTime.now().add(
-        Duration(
-          seconds: int.parse(body['expiresIn'])
-        )
-      );
+      _expiryDate =
+          DateTime.now().add(Duration(seconds: int.parse(body['expiresIn'])));
       notifyListeners();
     }
   }
-  
+
   Future<void> signup(String email, String password) async {
     return _authenticate(email, password, 'signUp');
   }
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  void logout() {
+    _token = null;
+    _email = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
   }
 }
 
