8caddca3533304bcd6aaf03c4acb120eabb9a809
Rafael <rafaelbrandao1992@gmail.com>
2023-06-27 17:26:58 -0300

    feat: take a picture

--- secao-13/my_locations/lib/components/image_input.dart
+++ secao-13/my_locations/lib/components/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,6 +11,20 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+  
+  _takePicture() async {
+    final ImagePicker _picker = ImagePicker();
+    XFile imageFile = await _picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    ) as XFile;
+
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -18,15 +35,20 @@ class _ImageInputState extends State<ImageInput> {
           decoration:
               BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
           alignment: Alignment.center,
-          child: const Text('Nenhuma imagem'),
+          child: _storedImage == null
+              ? const Text('Nenhuma imagem')
+              : Image.file(
+                  _storedImage as File,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
         ),
         const SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
-            onPressed: () {},
+            onPressed: _takePicture,
             style: TextButton.styleFrom(
-              foregroundColor: Theme.of(context).colorScheme.primary
-            ),
+                foregroundColor: Theme.of(context).colorScheme.primary),
             icon: const Icon(Icons.camera),
             label: const Text('Tirar foto'),
           ),
