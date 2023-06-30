f380031d93ada0879d78ce085358cf13343af883
Rafael <rafaelbrandao1992@gmail.com>
2023-06-30 16:47:53 -0300

    feat: create loading page

---
+++ secao-14/chat/lib/pages/loading_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(color: Theme.of(context).colorScheme.onPrimary),
+            const SizedBox(height: 10),
+            Text(
+              'Carregando...',
+              style: TextStyle(
+                color: Theme.of(context).primaryTextTheme.titleLarge?.color,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
