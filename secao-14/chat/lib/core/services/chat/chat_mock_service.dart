3b866bc687a193e436b5bdfacc1c8c64e65b4fa6
Rafael <rafaelbrandao1992@gmail.com>
2023-07-03 17:00:57 -0300

    feat: create services for chat

---
+++ secao-14/chat/lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'dart:async';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+      id: '1',
+      text: 'Bom dia',
+      createdAt: DateTime.now(),
+      userId: '123',
+      userName: 'Tião',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '2',
+      text: 'Bom dia, teremos reunião hoje',
+      createdAt: DateTime.now(),
+      userId: '234',
+      userName: 'Zé',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+    ChatMessage(
+      id: '3',
+      text: 'Bom dia, teremos sim',
+      createdAt: DateTime.now(),
+      userId: '456',
+      userName: 'Bia',
+      userImageURL: 'assets/images/avatar.png',
+    ),
+  ];
+
+  static MultiStreamController<List<ChatMessage>>? _controller;
+
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+    _controller?.add(_msgs);
+  });
+
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return _msgsStream;
+  }
+
+  @override
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+      id: Random().nextDouble().toString(),
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageURL,
+    );
+    _msgs.add(newMessage);
+    _controller?.add(_msgs);
+
+    return newMessage;
+  }
+}
