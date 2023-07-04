ec4511b8b82818e18924b67b6fa80b68c2af83e8
Rafael <rafaelbrandao1992@gmail.com>
2023-07-04 16:59:04 -0300

    feat: add future builder to home page

--- secao-14/chat/lib/pages/auth_or_app_page.dart
+++ secao-14/chat/lib/pages/auth_or_app_page.dart
@@ -4,24 +4,34 @@ import 'package:chat/pages/auth_page.dart';
 import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
   const AuthOrAppPage({super.key});
 
+  Future<void> init(BuildContext context) async {
+    await Firebase.initializeApp();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: StreamBuilder<ChatUser?>(
+    return FutureBuilder(
+        future: init(context),
         builder: (ctx, snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const LoadingPage();
           } else {
-            return snapshot.hasData ? const ChatPage() : const AuthPage();
+            return StreamBuilder<ChatUser?>(
+              builder: (ctx, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const LoadingPage();
+                } else {
+                  return snapshot.hasData ? const ChatPage() : const AuthPage();
+                }
+              },
+              stream: AuthService().userChanges,
+            );
           }
-        },
-        stream: AuthService().userChanges,
-      ),
-    );
+        });
   }
 }
