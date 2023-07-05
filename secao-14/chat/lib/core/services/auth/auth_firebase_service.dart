c6982ed3ac2d4dee1489c7af93f171efb12f658d
Rafael <rafaelbrandao1992@gmail.com>
2023-07-05 15:18:18 -0300

    feat: add image to firebase store

--- secao-14/chat/lib/core/services/auth/auth_firebase_service.dart
+++ secao-14/chat/lib/core/services/auth/auth_firebase_service.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AuthFirebaseService implements AuthService {
   static ChatUser? _currentUser;
@@ -38,8 +39,12 @@ class AuthFirebaseService implements AuthService {
 
     if (credential.user == null) return;
 
+    final imageName = '${credential.user!.uid}.jpg';
+    final imageURL = await _uploadUserImage(image, imageName);
+
     credential.user?.updateDisplayName(name);
-    // credential.user?.updatePhotoURL(photoURL);
+    credential.user?.updatePhotoURL(imageURL);
+
   }
 
   @override
@@ -53,6 +58,15 @@ class AuthFirebaseService implements AuthService {
   @override
   Future<void> logout() async {
     FirebaseAuth.instance.signOut();
+  }
+
+  Future<String?> _uploadUserImage(File? image, String imageName) async {
+    if(image == null) return null;
+
+    final storage = FirebaseStorage.instance;
+    final imageRef = storage.ref().child('user_images').child(imageName);
+    await imageRef.putFile(image).whenComplete(() {});
+    return await imageRef.getDownloadURL();
   }
 
   static ChatUser _toChatUser(User user) {
