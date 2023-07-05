870b515c6b9fa7f458c8b3805336722b73718f32
Rafael <rafaelbrandao1992@gmail.com>
2023-07-05 16:57:27 -0300

    refactor: to use conversor in chat Firebase service

--- secao-14/chat/lib/core/services/chat/chat_firebase_service.dart
+++ secao-14/chat/lib/core/services/chat/chat_firebase_service.dart
@@ -6,7 +6,6 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatFirebaseService implements ChatService {
-
   @override
   Stream<List<ChatMessage>> messagesStream() {
     return const Stream<List<ChatMessage>>.empty();
@@ -15,14 +14,53 @@ class ChatFirebaseService implements ChatService {
   @override
   Future<ChatMessage?> save(String text, ChatUser user) async {
     final store = FirebaseFirestore.instance;
-    await store.collection('chat').add({
-      'text': text,
-      'createdAt': DateTime.now().toIso8601String(),
-      'userId': user.id,
-      'userName': user.name,
-      'userImageURL': user.imageURL,
-    });
 
-    return null;
+    final msg = ChatMessage(
+      id: '',
+      text: text,
+      createdAt: DateTime.now(),
+      userId: user.id,
+      userName: user.name,
+      userImageURL: user.imageURL,
+    );
+
+    final docRef = await store
+        .collection('chat')
+        .withConverter(
+          fromFirestore: _fromFirestore,
+          toFirestore: _toFirestore,
+        )
+        .add(msg);
+
+    final doc = await docRef.get();
+
+    return doc.data()!;
+  }
+
+  ChatMessage _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> doc,
+    SnapshotOptions? options,
+  ) {
+    return ChatMessage(
+      id: doc.id,
+      text: doc['text'],
+      createdAt: DateTime.parse(doc['createdAt']),
+      userId: doc['userId'],
+      userName: doc['userName'],
+      userImageURL: doc['userImageURL'],
+    );
+  }
+
+  Map<String, dynamic> _toFirestore(
+    ChatMessage msg,
+    SetOptions? options,
+  ) {
+    return {
+      'text': msg.text,
+      'createdAt': msg.createdAt.toIso8601String(),
+      'userId': msg.userId,
+      'userName': msg.userName,
+      'userImageURL': msg.userImageURL,
+    };
   }
 }
