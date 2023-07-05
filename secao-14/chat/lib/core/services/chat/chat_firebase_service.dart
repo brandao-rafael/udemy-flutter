23437e6bf04eac751cd8b0388c98506726122d56
Rafael <rafaelbrandao1992@gmail.com>
2023-07-05 16:26:43 -0300

    feat: initial logics to save chat in firebase

---
+++ secao-14/chat/lib/core/services/chat/chat_firebase_service.dart
@@ -0,0 +1,28 @@
+import 'package:chat/core/models/chat_message.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatFirebaseService implements ChatService {
+
+  @override
+  Stream<List<ChatMessage>> messagesStream() {
+    return const Stream<List<ChatMessage>>.empty();
+  }
+
+  @override
+  Future<ChatMessage?> save(String text, ChatUser user) async {
+    final store = FirebaseFirestore.instance;
+    await store.collection('chat').add({
+      'text': text,
+      'createdAt': DateTime.now().toIso8601String(),
+      'userId': user.id,
+      'userName': user.name,
+      'userImageURL': user.imageURL,
+    });
+
+    return null;
+  }
+}
