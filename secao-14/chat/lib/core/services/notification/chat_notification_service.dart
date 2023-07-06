a767c701533315f6f7feaf846349480ffe53222e
Rafael <rafaelbrandao1992@gmail.com>
2023-07-06 14:57:48 -0300

    feat: finalizing push notification

--- secao-14/chat/lib/core/services/notification/chat_notification_service.dart
+++ secao-14/chat/lib/core/services/notification/chat_notification_service.dart
@@ -25,9 +25,9 @@ class ChatNotificationService with ChangeNotifier {
 
   // Push Notifications
   Future<void> init() async {
+    await _configureTerminated();
     await _configureForeground();
     await _configureBackground();
-    await _configureTerminated();
   }
 
   Future<bool> get _isAuthorized async {
