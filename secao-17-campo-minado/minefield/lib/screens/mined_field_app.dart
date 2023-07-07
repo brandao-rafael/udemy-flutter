051d6a0358abedf449ab7acfc0010e87b181cd03
Rafael <rafaelbrandao1992@gmail.com>
2023-07-07 17:25:34 -0300

    feat: create field widget

--- secao-17-campo-minado/minefield/lib/screens/mined_field_app.dart
+++ secao-17-campo-minado/minefield/lib/screens/mined_field_app.dart
@@ -1,21 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:minefield/components/field_widget.dart';
 import 'package:minefield/components/result_widget.dart';
+import 'package:minefield/models/explosion_exception.dart';
+import 'package:minefield/models/field.dart';
 
 class MinedFieldApp extends StatelessWidget {
   const MinedFieldApp({super.key});
 
-  _restart() {
+  void _restart() {
     print('restart');
+  }
+
+  void _open(Field field) {
+    print('open');
+  }
+
+  void _changeAppointement(Field field) {
+    print('change appointement');
   }
 
   @override
   Widget build(BuildContext context) {
+    Field neighbor1 = Field(line: 1, column: 0);
+    neighbor1.putMine();
+    Field neighbor2 = Field(line: 1, column: 1);
+    neighbor2.putMine();
+    Field field = Field(line: 0, column: 0);
+    field.addNeighbor(neighbor1);
+    field.addNeighbor(neighbor2);
+    
+    try {
+      // field.putMine();
+      field.changeAppointment();
+    } on ExplosionException catch (e) {
+      print(e);
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: ResultWidget(onRestart: _restart, win: null),
-        body: Container(
-          child: const Text('tabuleiro'),
-        ),
+        body: FieldWidget(
+          field: field,
+          onOpen: _open,
+          onChangeAppointement: _changeAppointement,
+        )
       ),
     );
   }
