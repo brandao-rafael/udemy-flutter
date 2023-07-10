1ffea518db5c744b42fe31f95d1355c5d5e675dc
Rafael <rafaelbrandao1992@gmail.com>
2023-07-10 14:54:05 -0300

    feat: get board size by screen size

--- secao-17-campo-minado/minefield/lib/screens/mined_field_app.dart
+++ secao-17-campo-minado/minefield/lib/screens/mined_field_app.dart
@@ -15,11 +15,7 @@ class MinedFieldApp extends StatefulWidget {
 class _MinedFieldAppState extends State<MinedFieldApp> {
   bool? _win;
 
-  final Board _board = Board(
-    lines: 12,
-    columns: 12,
-    amountOfBombs: 3,
-  );
+  Board _board = Board(columns: 1, lines: 1, amountOfBombs: 0);
 
   void _restart() {
     setState(() {
@@ -29,12 +25,12 @@ class _MinedFieldAppState extends State<MinedFieldApp> {
   }
 
   void _open(Field field) {
-    if(_win != null) return;
-    
+    if (_win != null) return;
+
     setState(() {
       try {
         field.open();
-        if(_board.solved) {
+        if (_board.solved) {
           _win = true;
         }
       } on ExplosionException {
@@ -45,13 +41,28 @@ class _MinedFieldAppState extends State<MinedFieldApp> {
   }
 
   void _changeAppointement(Field field) {
-    if(_win != null) return;
+    if (_win != null) return;
     setState(() {
       field.changeAppointment();
-      if(_board.solved) {
+      if (_board.solved) {
         _win = true;
       }
     });
+  }
+
+  Board _getBoard(double width, double height) {
+    if (_board.lines == 1) {
+      int amountOfColumns = 15;
+      double fieldSize = width / amountOfColumns;
+      int amountOfLines = (height / fieldSize).floor();
+
+      _board = Board(
+        lines: amountOfLines,
+        columns: amountOfColumns,
+        amountOfBombs: 33,
+      );
+    }
+    return _board;
   }
 
   @override
@@ -62,10 +73,15 @@ class _MinedFieldAppState extends State<MinedFieldApp> {
           win: _win,
           onRestart: _restart,
         ),
-        body: BoardWidget(
-          board: _board,
-          onOpen: _open,
-          onChangeAppointement: _changeAppointement,
+        body: LayoutBuilder(
+          builder: (ctx, constraints) => BoardWidget(
+            board: _getBoard(
+              constraints.maxWidth,
+              constraints.maxHeight,
+            ),
+            onOpen: _open,
+            onChangeAppointement: _changeAppointement,
+          ),
         ),
       ),
     );
