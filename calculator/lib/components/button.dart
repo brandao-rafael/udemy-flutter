8964e0fbd4b1f8a86426dbae4c00cbd827b885b8
Rafael <rafaelbrandao1992@gmail.com>
2023-07-10 15:49:04 -0300

    feat: create named constructors to button

--- calculator/lib/components/button.dart
+++ calculator/lib/components/button.dart
@@ -1,14 +1,34 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, constant_identifier_names
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 28, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+
   final String text;
   final bool big;
+  final Color color;
 
   const Button({
     Key? key,
     required this.text,
     this.big = false,
+    this.color = DEFAULT,
+  }) : super(key: key);
+
+  const Button.big({
+    Key? key,
+    required this.text,
+    this.big = true,
+    this.color = DEFAULT,
+  }) : super(key: key);
+
+  const Button.operation({
+    Key? key,
+    required this.text,
+    this.big = false,
+    this.color = OPERATION,
   }) : super(key: key);
 
   @override
@@ -16,8 +36,16 @@ class Button extends StatelessWidget {
     return Expanded(
       flex: big ? 2 : 1,
       child: ElevatedButton(
+        style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => color)),
         onPressed: () {},
-        child: Text(text),
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.w200,
+          ),
+        ),
       ),
     );
   }
