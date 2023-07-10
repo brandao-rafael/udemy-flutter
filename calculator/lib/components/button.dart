07c8523b7720f13412475cf22157e9603d2a2039
Rafael <rafaelbrandao1992@gmail.com>
2023-07-10 16:14:30 -0300

    feat: finalizing layout

--- calculator/lib/components/button.dart
+++ calculator/lib/components/button.dart
@@ -2,19 +2,21 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  static const DARK = Color.fromRGBO(82, 82, 28, 1);
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
   static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
   static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
 
   final String text;
   final bool big;
   final Color color;
+  final void Function(String) cb;
 
   const Button({
     Key? key,
     required this.text,
     this.big = false,
     this.color = DEFAULT,
+    required this.cb,
   }) : super(key: key);
 
   const Button.big({
@@ -22,6 +24,7 @@ class Button extends StatelessWidget {
     required this.text,
     this.big = true,
     this.color = DEFAULT,
+    required this.cb,
   }) : super(key: key);
 
   const Button.operation({
@@ -29,6 +32,7 @@ class Button extends StatelessWidget {
     required this.text,
     this.big = false,
     this.color = OPERATION,
+    required this.cb,
   }) : super(key: key);
 
   @override
@@ -36,8 +40,9 @@ class Button extends StatelessWidget {
     return Expanded(
       flex: big ? 2 : 1,
       child: ElevatedButton(
-        style: ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) => color)),
-        onPressed: () {},
+        style: ButtonStyle(
+            backgroundColor: MaterialStateColor.resolveWith((states) => color)),
+        onPressed: () => cb(text),
         child: Text(
           text,
           style: const TextStyle(
--- calculator/lib/screens/calculator.dart
+++ calculator/lib/screens/calculator.dart
@@ -1,16 +1,30 @@
 import 'package:calculator/components/display.dart';
 import 'package:calculator/components/keyboard.dart';
+import 'package:calculator/models/memory.dart';
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
   const Calculator({super.key});
 
   @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  void onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Column(children: [
-        Display('123.45'),
-        Keyboard(),
+        Display(memory.value),
+        Keyboard(cb: onPressed),
       ]),
     );
   }
