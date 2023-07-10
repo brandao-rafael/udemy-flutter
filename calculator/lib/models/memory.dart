7e688743492f85115291bb663677241b03e4b94c
Rafael <rafaelbrandao1992@gmail.com>
2023-07-10 17:59:06 -0300

    feat: implementing operations

--- calculator/lib/models/memory.dart
+++ calculator/lib/models/memory.dart
@@ -3,30 +3,58 @@ class Memory {
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String operation = '';
+  String? _operation = '';
   String _value = '0';
   bool _wipeValue = false;
+  String? _lastCommand;
 
   void applyCommand(String command) {
-    if(command == 'AC') {
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+    }
+    if (command == 'AC') {
       _allClear();
-    } else if (operations.contains(command)){
+    } else if (operations.contains(command)) {
       _setOperation(command);
     } else {
       _addDigit(command);
     }
+    _lastCommand = command;
+  }
+
+  bool _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
   }
 
   _setOperation(String newOperation) {
-    _wipeValue = true;
+    bool isEqualSign = newOperation == '=';
 
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
+    _wipeValue = !isEqualSign;
   }
 
   _addDigit(String digit) {
     final isDot = digit == '.';
     final wipeValue = (_value == '0' && !isDot) || _wipeValue;
 
-    if(isDot && _value.contains('.') && !wipeValue) return;
+    if (isDot && _value.contains('.') && !wipeValue) return;
 
     final emptyValue = isDot ? '0' : '';
     final currentValue = wipeValue ? emptyValue : _value;
@@ -34,11 +62,31 @@ class Memory {
     _wipeValue = false;
 
     _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
-    print(_buffer[_bufferIndex]);
   }
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _operation = null;
+    _bufferIndex = 0;
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 
   String get value {
