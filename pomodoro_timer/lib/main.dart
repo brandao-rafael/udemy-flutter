7454bd8e7f8dd196f6640731e36bd638b5d11180
Rafael <rafaelbrandao1992@gmail.com>
2023-07-11 16:27:36 -0300

    feat: create pomodoro store

--- pomodoro_timer/lib/main.dart
+++ pomodoro_timer/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_timer/pages/pomodoro.dart';
+import 'package:pomodoro_timer/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pomodoro',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
-        // useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (context) => PomodoroStore(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Pomodoro',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+          // useMaterial3: true,
+        ),
+        home: const Pomodoro(),
       ),
-      home: const Pomodoro(),
     );
   }
 }
--- pomodoro_timer/lib/pages/pomodoro.dart
+++ pomodoro_timer/lib/pages/pomodoro.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_timer/components/entry_time.dart';
 import 'package:pomodoro_timer/components/timer.dart';
+import 'package:pomodoro_timer/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final store = Provider.of<PomodoroStore>(context);
+    return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
+          const Expanded(
             child: Timer(),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(vertical: 40),
+            padding: const EdgeInsets.symmetric(vertical: 40),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 EntryTime(
-                  value: 25,
+                  value: store.workTime,
                   title: 'Trabalho',
                 ),
                 EntryTime(
-                  value: 5,
+                  value: store.restTime,
                   title: 'Descanso',
                 )
               ],
