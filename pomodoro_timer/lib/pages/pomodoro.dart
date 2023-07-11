5e429abbed8e6a8631b4a2cad986a4578da89f0e
Rafael <rafaelbrandao1992@gmail.com>
2023-07-11 15:53:18 -0300

    feat: initial timer component

--- pomodoro_timer/lib/pages/pomodoro.dart
+++ pomodoro_timer/lib/pages/pomodoro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro_timer/components/entry_time.dart';
+import 'package:pomodoro_timer/components/timer.dart';
 
 class Pomodoro extends StatelessWidget {
   const Pomodoro({super.key});
@@ -8,20 +9,26 @@ class Pomodoro extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('Pomodoro'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              EntryTime(
-                value: 25,
-                title: 'Trabalho',
-              ),
-              EntryTime(
-                value: 5,
-                title: 'Descanso',
-              )
-            ],
+          Expanded(
+            child: Timer(),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                EntryTime(
+                  value: 25,
+                  title: 'Trabalho',
+                ),
+                EntryTime(
+                  value: 5,
+                  title: 'Descanso',
+                )
+              ],
+            ),
           ),
         ],
       ),
